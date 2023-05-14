fcfefd16c350136100f2f36d0488523643ead874
tbussozungri <tbussozungri@itba.edu.ar>
2023-05-14 17:32:55 -0300

    Ya se pueden crear rutinas con parámetros

--- src/components/RoutinesContainer.vue
+++ src/components/RoutinesContainer.vue
@@ -27,37 +27,183 @@
           ></v-select>
         </v-card-text>
         <v-card-actions>
-          <v-btn :disabled="!isValidName()" color="primary" @click="createRoutine(routineName,devicesSelected,actionsSelected) && (showRoutine=false)">Crear</v-btn>
+          <v-btn :disabled="!isValidName()" color="primary" @click="createRoutine(routineName,devicesSelected,actionsSelected,paramsSelected) && (showRoutine=false)">Crear</v-btn>
           <v-btn @click="showRoutine = false">Cancelar</v-btn>
           <v-btn :disabled="devicesSelected.length <= 0" @click="configureDevices()">Configurar dispositivos</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
-
     <v-dialog v-model="setupDevices" width="700" height="auto">
       <v-card >
         <v-card-title>Seleccione las acciones a realizar</v-card-title>
-        <v-container >
-          <v-row >
-            <v-col cols="4">
-              <v-container v-for="device in devicesSelected" >
-                <v-card-text >Dispositivo:{{device.name}}</v-card-text>
-              </v-container>
+        <v-container v-for="(device,index) in devicesSelected" :key="index" >
+          <v-row>
+            <v-col cols="2" >
+              <v-card-text >{{device.name}}</v-card-text>
             </v-col>
-            <v-col cols="4" >
+            <v-col cols="3" >
               <v-container  >
-                <v-select v-for="(device,index) in devicesSelected" :key="index"
+                <v-select
                           v-model="actionsSelected[`${index}`]"
                           label="Acción"
                           :items="getActions(device.type.id)"
                 ></v-select>
               </v-container>
             </v-col>
-            <v-col cols="4" >
-              <v-container  >
-                <v-btn  color="red">Eliminar dispositivo </v-btn>
-              </v-container>
+            <v-col cols="5" v-if="needParams(actionsSelected[`${index}`])">
+              <v-form v-if="actionsSelected[`${index}`] === 'setVolume'" @submit.prevent="rules()">
+                <v-row>
+                  <v-col>
+                    <v-text-field
+                      v-model="paramsSelected[`${index}`]"
+                      label="Volumen"
+                      type="number"
+                      hint="Inserte el volumen deseado"
+                      :rules="[ ()=> paramsSelected[`${index}`] >= 0 && paramsSelected[`${index}`] <= 10 ? true : errorMessage]"
+                      :error-message="errorMessage"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-form>
+
+              <v-form v-if="actionsSelected[`${index}`] === 'setBrightness'" @submit.prevent="rules()">
+                <v-row>
+                  <v-col>
+                    <v-text-field
+                      v-model="paramsSelected[`${index}`]"
+                      label="Brillo"
+                      type="number"
+                      hint="Inserte el porcentaje de brillo deseado"
+                      :rules="[ ()=> paramsSelected[`${index}`] >= 0 && paramsSelected[`${index}`] <= 100 ? true : errorMessage]"
+                      :error-message="errorMessage"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-form>
+
+              <v-form v-if="actionsSelected[`${index}`] === 'setTemperature' && device.type.id === 'li6cbv5sdlatti0j'" @submit.prevent="rules()">
+                <v-row>
+                  <v-col>
+                    <v-text-field
+                      v-model="paramsSelected[`${index}`]"
+                      label="Temperatura"
+                      type="number"
+                      hint="Inserte el temperatura deseada"
+                      :rules="[ ()=> paramsSelected[`${index}`] >= 18 && paramsSelected[`${index}`] <= 38 ? true : errorMessage]"
+                      :error-message="errorMessage"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-form>
+
+              <v-form v-if="(actionsSelected[`${index}`] === 'setTemperature')&& device.type.id === 'rnizejqr2di0okho'" @submit.prevent="rules()">
+                <v-row>
+                  <v-col>
+                    <v-text-field
+                      v-model="paramsSelected[`${index}`]"
+                      label="Temperatura"
+                      type="number"
+                      hint="Inserte el temperatura deseada"
+                      :rules="[ ()=> paramsSelected[`${index}`] >= 2 && paramsSelected[`${index}`] <= 8 ? true : errorMessage]"
+                      :error-message="errorMessage"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-form>
+
+              <v-form v-if="actionsSelected[`${index}`] === 'setFreezerTemperature'" @submit.prevent="rules()">
+                <v-row>
+                  <v-col>
+                    <v-text-field
+                      v-model="paramsSelected[`${index}`]"
+                      label="Temperatura"
+                      type="number"
+                      hint="Inserte el temperatura deseada"
+                      :rules="[ ()=> paramsSelected[`${index}`] >= (-8) && paramsSelected[`${index}`] <= (-20) ? true : errorMessage]"
+                      :error-message="errorMessage"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-form>
+
+              <v-form v-if="actionsSelected[`${index}`] === 'setGenre'">
+                <v-select
+                  v-model="paramsSelected[`${index}`]"
+                  label="Seleccione el género"
+                  :items="setGenre"
+                ></v-select>
+              </v-form>
+
+              <v-form v-if="actionsSelected[`${index}`] === 'dispense' && dispenseParams(index)">
+
+                <v-text-field
+                  v-model="cant"
+                  label="Líquido"
+                  type="number"
+                  hint="Inserte el cantidad deseada"
+                ></v-text-field>
+
+                <v-select
+                  v-model="unit"
+                  label="Seleccione la unidad"
+                  :items="dispense"
+                ></v-select>
+              </v-form>
+
+              <v-form v-if="(actionsSelected[`${index}`] === 'setMode') && device.type.id === 'li6cbv5sdlatti0j'">
+                <v-select
+                  v-model="paramsSelected[`${index}`]"
+                  label="Seleccione el modo"
+                  :items="acsetMode"
+                ></v-select>
+              </v-form>
+
+              <v-form v-if="actionsSelected[`${index}`] === 'setVerticalSwing'">
+                <v-select
+                  v-model="paramsSelected[`${index}`]"
+                  label="Seleccione el modo de desplazamiento"
+                  :items="setVerticalSwing"
+                ></v-select>
+              </v-form>
+
+              <v-form v-if="actionsSelected[`${index}`] === 'setHorizontalSwing'">
+                <v-select
+                  v-model="paramsSelected[`${index}`]"
+                  label="Seleccione el modo de desplazamiento"
+                  :items="setHorizontalSwing"
+                ></v-select>
+              </v-form>
+
+              <v-form v-if="actionsSelected[`${index}`] === 'setFanSpeed'">
+                <v-select
+                  v-model="paramsSelected[`${index}`]"
+                  label="Seleccione la velocidad del ventilador"
+                  :items="setFanSpeed"
+                ></v-select>
+              </v-form>
+
+              <v-form v-if="(actionsSelected[`${index}`] === 'setMode') && device.type.id === 'rnizejqr2di0okho'">
+                <v-select
+                  v-model="paramsSelected[`${index}`]"
+                  label="Seleccione la velocidad del ventilador"
+                  :items="fridgesetMode"
+                ></v-select>
+              </v-form>
             </v-col>
+            <v-col v-else-if="noParams(index)">
+              <v-list-item title="Esta acción no recibe parámetros"></v-list-item>
+            </v-col>
+           <v-container>
+             <v-col cols="2" >
+               <v-btn
+                 icon="mdi-delete"
+                 variant="text"
+                 color="error"
+                 class="close"
+                 @click="deleteDevice(device.name)">
+               </v-btn>
+             </v-col>
+           </v-container>
           </v-row>
         </v-container>
         <v-card-actions>
@@ -71,6 +217,9 @@
       <v-row>
         <v-col v-for="routine in routines" cols="4" >
           <v-card class="routineCard" >
+            <h1 v-for="param in paramsSelected">{{param}}</h1>
+            <h1> Unidad:{{unit}}</h1>
+            <h1> Cantidad : {{cant}}</h1>
             <v-card-text>
               <v-card-title >{{routine.name}}</v-card-title>
               <v-list>
@@ -97,7 +246,7 @@
 import { UseRoutineStore } from "@/store/routineStore.js"
 import { useDeviceStore } from "@/store/deviceStore.js"
 import { Routine, RoutineActions, RoutineMeta } from "@/api/routine";
-import { computed, onMounted, ref } from "vue";
+import {computed, onMounted, ref, toRaw} from "vue";
 
 
 const routineStore = UseRoutineStore()
@@ -112,7 +261,6 @@ const routineName = ref('')
 let storeSelected = ref([])
 const devicesSelected = ref([])
 const actionsSelected = ref([])
-const actions = ref([])
 
 const speakerActions = ["setVolume","play","stop","pause","resume","nextSong","previousSong","setGenre","getPlaylist"]
 const faucetActions = ["open","close","dispense"]
@@ -121,22 +269,24 @@ const acActions = ["turnOn","turnOff","setTemperature","setMode","setVerticalSwi
 const fridgeActions = ["setFreezerTemperature","setTemperature","setMode"]
 
 const setGenre= ["clasica","country","dance","latina","pop","rock"]
-const dispense= ["mililitros","centilitros","decilitros","litros"]
+const dispense= ["ml","cl","dl","l","dal","hl","kl"]
 const acsetMode = ["ventilación","frio","calor"]
 const setVerticalSwing = ["automatico","22","45","67","90"]
 const setHorizontalSwing = ["automatico","-90","-45","0","45","90"]
 const setFanSpeed = ["automatico","25","50","75","100"]
 const fridgesetMode = ["normal","fiesta","vacaciones"]
 
-
-const param = ref('')
+const errorMessage = computed( () => !isNaN(Number(text.value)) && text.value > 0 ? "" : !isNaN(Number(text.value)) && text.value <= 0 ? 'Debe ingresar un número mayor a cero' : 'Debe ingresar un numero')
+const unit = ref('')
+const cant = ref('')
+let paramsSelected = ref([])
 
 const asyncSpeakers = computed(() => devices.value.filter((device) => device.type.id === devicesTypes.value[0].id))
 const asyncFaucet = computed(() => devices.value.filter((device) => device.type.id === devicesTypes.value[1].id))
 const asyncLamp = computed(() => devices.value.filter((device) => device.type.id === devicesTypes.value[2].id))
 const asyncAc = computed(() => devices.value.filter((device) => device.type.id === devicesTypes.value[3].id))
 const asyncFridge = computed(() => devices.value.filter((device) => device.type.id === devicesTypes.value[4].id))
-
+const text = ref("")
 const isLoading = ref(true)
 const devicesTypes = ref([
   {
@@ -179,6 +329,10 @@ function configureDevices(){
   setupDevices.value = true
 
 }
+function needParams(action){
+  return !(action === 'play' || action === 'stop' || action === 'pause' || action === 'resume' || action === 'nextSong' || action === 'previousSong' || action === 'getPlaylist'
+    || action === 'open' || action === 'close' || action === 'turnOn' || action === 'turnOff');
+}
 function getIcon(id){
   return devicesTypes.value.find((type) => type.id === id).icon
 }
@@ -186,6 +340,30 @@ function isValidName(){
   return routineName.value.length >0
 }
 
+function noParams(index){
+  paramsSelected.value[index] = "noparams"
+  return true
+}
+function dispenseParams(index){
+  paramsSelected.value[index] = "dispense"
+  return true
+}
+
+function rules(){
+  return !numberRule() || text.value === ''
+}
+
+function deleteDevice(name) {
+  const index = devicesSelected.value.findIndex(device => device.name === name);
+  devicesSelected.value.splice(index, 1);
+  if (devicesSelected.value.length === 0){
+    setupDevices.value = false
+  }
+}
+
+function numberRule(){
+  return !isNaN(Number(text.value)) && text.value > 0 && text.value <= 100
+}
 function getActions(typeId){
   return devicesTypes.value.find((type) => type.id === typeId ).actions
 }
@@ -198,12 +376,12 @@ async function executeRoutine(id){
     console.log(error)
   }
 }
-
-async function createRoutine(routineName,devicesSelected,actionsSelected){
+async function createRoutine(routineName,devicesSelected,actionsSelected,paramsSelected){
 
   const routineIDs = []
   const deviceActions = []
   let routineActions = []
+  const paramestros = []
   const routineMeta = new RoutineMeta()
 
   for (const device of devicesSelected) {
@@ -214,8 +392,20 @@ async function createRoutine(routineName,devicesSelected,actionsSelected){
     deviceActions.push(action.toString())
   }
 
+  for (let param of paramsSelected) {
+    if (param === "dispense"){
+      paramestros.push([cant.value.toString(),unit.value.toString()])
+    }
+    else if (param === "noparams"){
+      paramestros.push([])
+    }
+    else {
+      paramestros.push([param])
+    }
+  }
+
   for (let i = 0; i <routineIDs.length ; i++) {
-    routineActions.push(new RoutineActions(routineIDs[i],deviceActions[i],[],routineMeta))
+    routineActions.push(new RoutineActions(routineIDs[i],deviceActions[i],paramestros[i],routineMeta))
   }
   const _routine = new Routine(routineName,routineActions,routineMeta)
 
